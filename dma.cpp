1a013d23ad51088c00593cadd61daf40a0a57269
dipeshbhatta02-cmyk <dipeshbhatta02@gmail.com>
2026-06-08 09:12:05 +0545

    Add files via upload

---
+++ dma.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *ptr = new int;
+
+    *ptr = 100;
+
+    cout << "Value = " << *ptr << endl;
+
+    delete ptr;
+
+
+    return 0;
+}
