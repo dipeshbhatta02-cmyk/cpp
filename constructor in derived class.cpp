8af999d5e783c93859753357da0a89e25d95c613
dipeshbhatta02-cmyk <dipeshbhatta02@gmail.com>
2026-06-29 09:40:35 +0545

    Add files via upload

--- constructor in derived class.cpp
+++ constructor in derived class.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout << "Base class constructor" << endl;
+    }
+
+    ~Base() {
+        cout << "Base class destructor" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived class constructor" << endl;
+    }
+
+    ~Derived() {
+        cout << "Derived class destructor" << endl;
+    }
+};
+
+int main() {
+    Derived obj;
+    return 0;
+}
 
